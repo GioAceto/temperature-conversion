b8700559024a019d7bc67021508d809f7a5faa67
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-08-03 19:36:06 -0400

    add 2 additional tests fo rthe getCelsius function

--- tests.js
+++ tests.js
@@ -5,7 +5,11 @@ const getCelsius = require('./calculation');
 describe('getCelsius', () => {
   it('should convert the provided number in fahrenheit to celsius', () => {
     let testNum = 32
+    let testNum2 = -4
+    let testNum3 = 84
 
     expect(getCelsius(testNum)).to.equal(0)
+    expect(getCelsius(testNum2)).to.equal(-20)
+    expect(getCelsius(testNum3)).to.equal(29)
   })
 })
